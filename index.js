f08d25d872c441034facef185c12f06cc098da30
irena <irena.vent@gmail.com>
2021-12-15 11:37:04 +0100

    comments

--- index.js
+++ index.js
@@ -42,16 +42,20 @@ const stratify = d3
     .id((d) => d.name)
     .parentId((d) => d.parent);
 
+// we use sum to determinate the value which we gonna use in pack generator (a 3d function), to determinate how big should be.
 const rootNode = stratify(data).sum((d) => d.amount);
 
+// pack generator
 const pack = d3.pack().size([960, 700]).padding(5);
 
+// console.log(pack(rootNode)); - 3d generate a r, x and y property for all of the data
+
+// create an array of data
 const bubbleData = pack(rootNode).descendants();
 
 const color = d3.scaleOrdinal(["#d1c4e9", "#b39ddb", "#9575cd"]);
 
 // join data anda add group for each node
-
 const nodes = graph
     .selectAll("g")
     .data(bubbleData)
@@ -59,18 +63,20 @@ const nodes = graph
     .append("g")
     .attr("transform", (d) => `translate(${d.x},${d.y})`);
 
+// append a circle to the each group
 nodes
     .append("circle")
     .attr("r", (d) => d.r)
     .attr("stroke", "white")
-    .attr("stroke-width", 2)
+    .attr("stroke-width", 1)
     .attr("fill", (d) => color(d.depth));
 
+// append text to the small circle without any children
 nodes
     .filter((d) => !d.children)
     .append("text")
     .attr("text-anchor", "middle")
     .attr("dy", "0.3em")
     .attr("fill", "white")
-    .attr("font-size", (d) => d.value * 5)
+    .style("font-size", (d) => d.value * 5)
     .text((d) => d.data.name);
